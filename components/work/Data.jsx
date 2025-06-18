18d0247424eaadc15982ab7c0a8c0406e4189284
lucky-panchal <luckykanti31122006@gmail.com>
2025-06-18 17:20:58 +0530

    Modified Work file and project section updated

--- components/work/Data.jsx
+++ components/work/Data.jsx
@@ -1,28 +1,29 @@
 
 import Work4 from '../../src/assets/Work4.png';
-import Work5 from '../../src/assets/Work5.png';
+// import Work5 from '../../src/assets/Work5.png';
+import Work6 from '../../src/assets/Work6.jpg'
 
 export const projectsData = [
 
 	{
 		id: 1,
 		image: Work4,
-		title: 'Portfolio Website',
+		title: 'PortFolio',
 		category: 'web',
 		demoUrl: 'https://lackilohar.netlify.app/',
 		status: 'completed',
 	},
 	{
 		id: 2,
-		image: Work5,
-		title: 'NotDecided',
-		category: 'web',	
+		image: Work6,
+		title: 'Manorath',
+		category: 'app',
 		status: 'development',
 	},
 	{
 		id: 3,
-		image: Work5,
-		title: 'ColdHeart',
+		image: Work6,
+		title: 'AI-ML Based Project',
 		category: 'web',		
 		status: 'development',
 	}
