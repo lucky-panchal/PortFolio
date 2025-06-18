03cc745091a959fd3abdcbe0f2d49c02d3b3840d
lucky-panchal <luckykanti31122006@gmail.com>
2025-06-18 18:04:55 +0530

    New courseworks added

--- components/skills/Coursework.jsx
+++ components/skills/Coursework.jsx
@@ -1,70 +1,99 @@
-const Coursework = () => {
-  return (
-    <div className="skills__content">
-        <h3 className="skills__title">Coursework & Tools</h3>
 
-        <div className="skills__box">
-            <div className="skills__group">
-                <div className="skills__data">
-                    <i className='bx bxl-python' ></i>
 
-                    <div>
-                        <h3 className="skills__name">Python</h3>
-                        <span className="skills__level">Intermediate</span>
-                    </div>
-                </div>
+ const Coursework = () => {
+    return (
+        <div className="skills__content">
+  <h3 className="skills__title">Coursework & Tools</h3>
 
-                <div className="skills__data">
-                    <i className='bx bxl-c-plus-plus' ></i>
-
-                    <div>
-                        <h3 className="skills__name">C++</h3>
-                        <span className="skills__level">Advance</span>
-                    </div>
-                </div>
-
-                <div className="skills__data">
-                    <i className='bx bxl-java' ></i>
-
-                    <div>
-                        <h3 className="skills__name">Java</h3>
-                        <span className="skills__level">Basic</span>
-                    </div>
-                </div>
-            </div>
-
-            <div className="skills__group">
-
-                <div className="skills__data">
-                    <i className='bx bxl-gitlab' ></i>
-
-                    <div>
-                        <h3 className="skills__name">GitHub</h3>
-                        <span className="skills__level">Advance</span>
-                    </div>
-                </div>
-
-                <div className="skills__data">
-                    <i className='bx bxl-visual-studio' ></i>
-
-                    <div>
-                        <h3 className="skills__name">VS Code</h3>
-                        <span className="skills__level">Intermediate</span>
-                    </div>
-                </div>
-
-                <div className="skills__data">
-                    <i className='bx bxl-github' ></i>
-
-                    <div>
-                        <h3 className="skills__name">Figma</h3>
-                        <span className="skills__level">Intermediate</span>
-                    </div>
-                </div>
-            </div>
+  <div className="skills__box">
+    <div className="skills__group">
+      <div className="skills__data">
+        <i className='bx bxl-python'></i>
+        <div>
+          <h3 className="skills__name">Python</h3>
+          <span className="skills__level">Intermediate</span>
         </div>
+      </div>
+
+      <div className="skills__data">
+        <i className='bx bxl-c-plus-plus'></i>
+        <div>
+          <h3 className="skills__name">C++</h3>
+          <span className="skills__level">Advance</span>
+        </div>
+      </div>
+
+      <div className="skills__data">
+        <i className='bx bxl-java'></i>
+        <div>
+          <h3 className="skills__name">Java</h3>
+          <span className="skills__level">Basic</span>
+        </div>
+      </div>
+
+      <div className="skills__data">
+        <i className='bx bxs-cog'></i>
+        <div>
+          <h3 className="skills__name">Operating Systems</h3>
+          <span className="skills__level">Intermediate</span>
+        </div>
+      </div>
+
+      <div className="skills__data">
+        <i className='bx bx-layer'></i>
+        <div>
+          <h3 className="skills__name">OOPs</h3>
+          <span className="skills__level">Intermediate</span>
+        </div>
+      </div>
     </div>
-  )
+
+    <div className="skills__group">
+      <div className="skills__data">
+        <i className='bx bxl-github'></i>
+        <div>
+          <h3 className="skills__name">GitHub</h3>
+          <span className="skills__level">Advance</span>
+        </div>
+      </div>
+
+      <div className="skills__data">
+        <i className='bx bxl-visual-studio'></i>
+        <div>
+          <h3 className="skills__name">VS Code</h3>
+          <span className="skills__level">Intermediate</span>
+        </div>
+      </div>
+
+      <div className="skills__data">
+        <i className='bx bxl-figma'></i>
+        <div>
+          <h3 className="skills__name">Figma</h3>
+          <span className="skills__level">Intermediate</span>
+        </div>
+      </div>
+
+      <div className="skills__data">
+    <i className='bx bxs-data'></i>
+    <div>
+        <h3 className="skills__name">DBMS</h3>
+        <span className="skills__level">Intermediate</span>
+    </div>
+</div>
+
+
+      <div className="skills__data">
+        <i className='bx bx-bug'></i>
+        <div>
+          <h3 className="skills__name">VS Code Debugging</h3>
+          <span className="skills__level">Intermediate</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+    )
 }
 
 export default Coursework
