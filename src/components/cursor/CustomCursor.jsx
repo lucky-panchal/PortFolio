0d628ca7630ad8cad6efc2fc053a9c908a19fab8
lucky-panchal <luckykanti31122006@gmail.com>
2025-06-21 10:21:41 +0530

    CursorCustom file changedd

--- src/components/cursor/CustomCursor.jsx
+++ src/components/cursor/CustomCursor.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef } from 'react';
 import './CustomCursor.css';
-
 const CustomCursor = () => {
   const cursorRef = useRef(null);
   const mouseRef = useRef({ x: 0, y: 0 });
