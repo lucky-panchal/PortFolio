beca35d6ecde7ff6b8826dae927593373757676f
luckykanti31122006@gmail.com <luckykanti31122006@gmail.com>
2025-07-13 10:23:55 +0530

    Onne  Revview Updated And One  image updated

--- components/testimonials/Testimonials.jsx
+++ components/testimonials/Testimonials.jsx
@@ -63,7 +63,7 @@ const Testimonials = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.2 }}
       >
-        Testimonial
+        Testimonials
       </motion.span>
 
       <div className="testimonial__container">
