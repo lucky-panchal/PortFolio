4031074402524ed6187e2d09714cc32a14697bcd
luckykanti31122006@gmail.com <luckykanti31122006@gmail.com>
2025-07-13 10:27:42 +0530

    Photoss Filter Updation

--- components/testimonials/Data.jsx
+++ components/testimonials/Data.jsx
@@ -13,9 +13,9 @@ export const Data = [
     {
       id: 3,
       image: Image3,
-      title: "Urvi Panchal",
+      title: "Nidhi SK",
       description:
-        "Lacki always find himself curious about new things and always coming back with ideas to improve products and services. He is also working on growing the Learn Program and in the process, he started his Journey. He is very intelligent in terms  of social things.  All the best for your upcoming formulations, Lacki will be a great asset to any organization.",
+        "I’ve had the chance to work closely with Lacki, and his mix of creativity and technical skill never fails to impress me. Beyond being a talented developer and designer, he brings positivity and dedication that truly lift any team. As a good friend, I admire how he turns challenges into opportunities to grow. It’s rare to find someone who blends professionalism with genuine warmth so effortlessly.",
     },
    {
       id: 2,
@@ -24,4 +24,5 @@ export const Data = [
       description:
         "He is hardworking and capable of learning things quickly. He consistently demonstrates a proactive attitude and a strong willingness to take on new challenges. His ability to adapt to new technologies and concepts makes him a valuable asset to any team. He also shows great attention to detail and maintains a high level of commitment to completing tasks efficiently and effectively.",
     },
+    
   ];
