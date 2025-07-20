b26a91dbe80e9954201b5b44a01f885b56615c8a
luckykanti31122006@gmail.com <luckykanti31122006@gmail.com>
2025-07-20 12:29:23 +0530

    New Project WanderScape Added

--- components/work/Data.jsx
+++ components/work/Data.jsx
@@ -1,4 +1,4 @@
-
+import Work3 from '../../src/assets/Work3.png';
 import Work4 from '../../src/assets/Work4.png';
 import Work5 from '../../src/assets/Work5.png';
 import Work6 from '../../src/assets/Work6.jpg'
@@ -8,7 +8,7 @@ export const projectsData = [
 	{
 		id: 1,
 		image: Work4,
-		title: 'PortFolio-React-Based',
+		title: 'PortFolio',
 		category: 'web',
 		demoUrl: 'https://lackilohar.netlify.app/',
 		status: 'completed',
@@ -22,10 +22,11 @@ export const projectsData = [
 	},
 	{
 		id: 3,
-		image: Work6,
-		title: 'AI-ML Based Project',
-		category: 'web',		
-		status: 'coming_soon',
+		image: Work3,
+		title: 'WanderScape',
+		// demoUrl: 'https://github.com/lucky-panchal/WanderScape-backend.gi',
+		category: 'web',
+		status: 'development',
 	},
 	{
 		id: 4,
