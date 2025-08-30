232cf59150a49a012c9a6228b63f6ad4344a29a8
lucky-panchal <luckykanti31122006@gmail.com>
2025-08-30 18:49:45 +0530

    Remove terminal feature and restore original home background

--- components/testimonials/Testimonials.jsx
+++ components/testimonials/Testimonials.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import './testimonial.css';
-import './testimonial-3d.css';
 import { Data } from './Data';
 
 const Testimonials = () => {
