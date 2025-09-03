d2caa78067910dcaaa64d113dc17e0c8357a62d9
lucky-panchal <luckykanti31122006@gmail.com>
2025-09-03 20:22:51 +0530

    Redesign social icons with modern circular design, glassmorphism effects, and brand-specific hover animations

--- components/home/Social.jsx
+++ components/home/Social.jsx
@@ -4,21 +4,19 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="https://www.instagram.com/luckyp4nch4l/" className="home__social-icon" target="_blank">
+        <a href="https://www.instagram.com/luckyp4nch4l/" className="home__social-icon instagram" target="_blank" rel="noopener noreferrer">
             <i className="uil uil-instagram"></i>
+            <span className="social-tooltip">Instagram</span>
         </a>
 
-
-        {/* <a href="www.linkedin.com/in/lacki-lohar-463a23321" className="home__social-icon" target="_blank">
-            <i className="uil uil-linkedin"></i>
-        </a> */}
-
-        <a href="https://github.com/lucky-panchal" className="home__social-icon" target="_blank">
+        <a href="https://github.com/lucky-panchal" className="home__social-icon github" target="_blank" rel="noopener noreferrer">
             <i className="uil uil-github-alt"></i>
+            <span className="social-tooltip">GitHub</span>
         </a>
 
-        <a href="https://linkedin.com/in/lacki-lohar-463a23321" className="home__social-icon" target="_blank">
+        <a href="https://linkedin.com/in/lacki-lohar-463a23321" className="home__social-icon linkedin" target="_blank" rel="noopener noreferrer">
             <i className="uil uil-linkedin"></i>
+            <span className="social-tooltip">LinkedIn</span>
         </a>
     </div>
   )
